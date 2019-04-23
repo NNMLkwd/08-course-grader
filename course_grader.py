e7fe7d2d82c83984d291444187f70d2a507011b0
NNMLkwd <41547837+NNMLkwd@users.noreply.github.com>
2019-04-23 07:55:10 -0600

    Update course_grader.py

--- course_grader.py
+++ course_grader.py
@@ -1,2 +1,10 @@
-def course_grader(param):
-    return ""
+def course_grader(test_scores):
+   
+    average_score = sum(test_scores)/len(test_scores)
+    if all(avg_grade >= 50 and average_score >= 70 for avg_grade in test_scores):
+        return "Pass"
+        return "pass"
+
+    else:
+        return "Fail"
+        return "fail"
